7151d3640f9cac55c81c7733e4f89bf78b8838dc
Vigneash Sundar <vigneashsundar@live.com>
2015-01-24 02:13:40 +0530

    Working Coin collector :+1:

--- js/game.js
+++ js/game.js
@@ -45,6 +45,7 @@ var game = {
 		
 		// add our player entity in the entity pool
 		me.pool.register("mainPlayer", game.PlayerEntity);
+        me.pool.register("CoinEntity", game.CoinEntity);
 			
 		// enable the keyboard
 		me.input.bindKey(me.input.KEY.LEFT,		"left");
--- js/resources.js
+++ js/resources.js
@@ -2,6 +2,8 @@ game.resources = [
     //tile set
     {name:  "area01_level_tiles",type:"image",src:"data/img/map/area01_level_tiles.png"},
     {name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
+    {name:"spinning_coin_gold", type:"image", src:"data/img/sprite/spinning_coin_gold.png"},
+    {name:"wheelie_right",type:"image",src:"data/img/sprite/wheelie_right.png"},
     {name:"area01",type:"tmx",src:"data/map/workout.tmx"}
     /* Graphics.
      * @example
