882c3078f7f20b5b6bd0a0690813dea5723a2c5b
Vigneash Sundar <vigneashsundar@live.com>
2015-01-24 23:18:14 +0530

    Custom character revert!

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -1,69 +1,75 @@
 /**
- * a HUD container and child items
- */
-
+* a HUD container and child items
+*/
+ 
 game.HUD = game.HUD || {};
-
-
+ 
+ 
 game.HUD.Container = me.Container.extend({
-
-    init: function() {
-        // call the constructor
-        this._super(me.Container, 'init');
-
-        // persistent across level change
-        this.isPersistent = true;
-
-        // make sure we use screen coordinates
-        this.floating = true;
-
-        // make sure our object is always draw first
-        this.z = Infinity;
-
-        // give a name
-        this.name = "HUD";
-
-        // add our child score object at the top left corner
-        this.addChild(new game.HUD.ScoreItem(5, 5));
-    }
+ 
+  init: function() {
+    // call the constructor
+    this._super(me.Container, 'init');
+     
+    // persistent across level change
+    this.isPersistent = true;
+ 
+    // make sure we use screen coordinates
+    this.floating = true;
+ 
+    // make sure our object is always draw first
+    this.z = Infinity;
+ 
+    // give a name
+    this.name = "HUD";
+     
+    // add our child score object at the right-bottom position
+    this.addChild(new game.HUD.ScoreItem(630, 440));
+  }
 });
-
-
-/**
- * a basic HUD item to display score
- */
-game.HUD.ScoreItem = me.Renderable.extend({
-    /**
-     * constructor
-     */
-    init: function(x, y) {
-
-        // call the parent constructor
-        // (size does not matter here)
-        this._super(me.Renderable, 'init', [x, y, 10, 10]);
-
-        // local copy of the global score
-        this.score = -1;
-    },
-
-    /**
-     * update function
-     */
-    update : function () {
-        // we don't do anything fancy here, so just
-        // return true if the score has been updated
-        if (this.score !== game.data.score) {
-            this.score = game.data.score;
-            return true;
-        }
-        return false;
-    },
-
-    /**
-     * draw the score
-     */
-    draw : function (context) {
-        // draw it baby !
+ 
+/** 
+* a basic HUD item to display score
+*/
+ 
+game.HUD.ScoreItem = me.Renderable.extend({    
+ 
+  /** 
+   * constructor
+   */
+  init: function(x, y) {
+   
+    // call the parent constructor 
+    // (size does not matter here)
+    this._super(me.Renderable, 'init', [x, y, 10, 10]); 
+      
+      this.font = new me.BitmapFont("32x32_font",32);
+      this.font.set("right");
+ 
+     
+    // local copy of the global score
+    this.score = -1;
+      this.floating = true;
+  },
+ 
+  /**
+   * update function
+   */
+  update : function (dt) {
+    // we don't do anything fancy here, so just
+    // return true if the score has been updated
+    if (this.score !== game.data.score) {   
+      this.score = game.data.score;
+      return true;
     }
-
+    return false;
+  },
+ 
+  /**
+  * draw the score
+  */
+  draw : function (renderer) {
+    // draw it baby !
+       this.font.draw (renderer, game.data.score, this.pos.x, this.pos.y);
+  }
 });
--- js/entities/entities.js
+++ js/entities/entities.js
@@ -148,7 +148,7 @@ game.CoinEntity = me.CollectableEntity.extend({
   // an object is touched by something (here collected)
   onCollision : function (response, other) {
     // do something when collected
- 
+        game.data.score +=200;
     // make sure it cannot be collected "again"
     this.body.setCollisionMask(me.collision.types.NO_OBJECT);
  
